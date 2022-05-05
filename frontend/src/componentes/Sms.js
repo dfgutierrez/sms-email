2002d2326945d196ceea421d64d469e1030d42dd
Diego Gutierrez <Algar2021*>
2022-05-05 10:56:55 -0500

    the design was adjusted

--- frontend/src/componentes/Sms.js
+++ frontend/src/componentes/Sms.js
@@ -19,25 +19,52 @@ function withParams(Component) {
 class Crear extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { 
+      phones:[],
+      phone:"",
+      message:"",
+      hora:""
+  }
   }
   componentDidMount(){
+    const hoy = new Date();
+    this.setState({phones:[{id:1,phone:"3208654121"},{id:2,phone:"3208654122"},{id:3,phone:"3208654123"}]});
+    this.state.hora= hoy.getHours() + ':' + hoy.getMinutes();
+    this.state.hora= hoy.toLocaleString([], {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+  }
+  enviarDatos=(e)=>{
+    e.preventDefault();
 
-
-
+    document.getElementById("cardfooter").style.display='none';
+    document.getElementById('phone').disabled = true;
+    document.getElementById("messagesent").style.display='block';
 
   }
+  cambioValor=(e)=>{
+    const state=this.state;
+    state[e.target.name]=e.target.value;
+    this.setState({state});
+  }
   render() {
+    const{phone,message,phones}=this.state
     return (
-
+        
         <div className="container mt-4">
           <div className="card mx-auto">
+          <form onSubmit={this.enviarDatos}>
             <div className="card-header bg-transparent">
               <div className="navbar navbar-expand p-0">
-                <select className="form-control" name="" id="">
-                  <option className={"3208654123"}>3208654123</option>
-                  <option className={"3208654123"}>3208654123</option>
-                  <option className={"3208654123"}>3208654123</option>
+
+                <select className="form-control" name="phone" id="phone" value={phone} onChange={this.cambioValor} >
+                {phones.map(
+                  (phon)=>(
+                      <option key={phon.id}>{phon.phone}</option>
+                  )
+                )}
+                  
                 </select>
               </div>
             </div>
@@ -45,46 +72,52 @@ class Crear extends React.Component {
               className="card-body p-4"
               style={{ height: "400px", overflow: "auto" }}
             >
-              <div className="d-flex align-items-baseline text-end justify-content-end mb-4">
-                <div className="pe-2">
-                  <div>
-                    <div className="card card-text d-inline-block p-2 px-3 m-1">
-                      Let me know when you're available?
+              <div id="messagesent" style={{display:"none"}}>
+                <div className="d-flex align-items-baseline text-end justify-content-end mb-4" >
+                  <div className="pe-2"  >
+                    <div>
+                      <div className="card card-text d-inline-block p-2 px-3 m-1">
+                        {message}
+                      </div>
+                    </div>
+                    <div>
+                      <div className="small">{this.state.hora}</div>
                     </div>
                   </div>
-                  <div>
-                    <div className="small">01:13PM</div>
+                  <div className="position-relative avatar" >
+                    <img
+                      src={micalro}
+                      className="img-fluid rounded-circle"
+                      alt=""
+                    />
+                    <span className="position-absolute bottom-0 start-100 translate-middle p-1 bg-success border border-light rounded-circle">
+                      <span className="visually-hidden">New alerts</span>
+                    </span>
                   </div>
-                </div>
-                <div className="position-relative avatar">
-                  <img
-                    src={micalro}
-                    className="img-fluid rounded-circle"
-                    alt=""
-                  />
-                  <span className="position-absolute bottom-0 start-100 translate-middle p-1 bg-success border border-light rounded-circle">
-                    <span className="visually-hidden">New alerts</span>
-                  </span>
                 </div>
               </div>
             </div>
 
-
-            <div className="card-footer bg-white position-absolute w-100 bottom-0 m-0 p-1">
+            <div id="cardfooter" className="card-footer bg-white position-absolute w-100 bottom-0 m-0 p-1">
               <div className="input-group">
                 <input
+                  required
                   type="text"
                   className="form-control border-0"
-                  placeholder="Write a message..."
+                  placeholder="Escribe el mensaje ..."
+                  name="message" 
+                  id="message" 
+                  value={message} 
+                  onChange={this.cambioValor} 
                 />
                 <div className="input-group-text bg-transparent border-0">
-                  <button className="btn btn-light text-secondary">
+                  <button type="submit" className="btn btn-light text-secondary" >
                     <i className="fas fa-paper-plane"></i>
                   </button>
                 </div>
               </div>
             </div>
-
+            </form>
           </div>
         </div>
 
